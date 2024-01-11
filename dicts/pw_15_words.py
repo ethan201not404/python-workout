6e52b0845933f10ec336af84c04a7519b99ec23f
ethan201not404 <ethanli202020@gmail.com>
2024-01-11 09:12:00 -0500

    fix typo

--- dicts/pw_15_words.py
+++ dicts/pw_15_words.py
@@ -7,8 +7,8 @@ That is, how many three-letter words, four-letter words, five-letter words, and 
 Display your results.
 """
 
-from collections import defaultdict
 import os
+from collections import defaultdict
 
 words_db = defaultdict(int)
 
@@ -19,9 +19,8 @@ with open(f"{cmd}/dicts/{file}") as f_reader:
     lines = f_reader.readlines()
     for line in lines:
         words_in_line = line.strip().split()
-        # print(words_in_line, len(words_in_line))
         for word in words_in_line:
             words_db[len(word)] += 1
 
 for k, v in words_db.items():
-    print(f"word lenth of {k} has occurrences {v}")
+    print(f"word length of {k} has occurrences {v}")
